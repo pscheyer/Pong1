14ae70c253aade01990bbb9652dba01943d76227
Peter Scheyer <pscheyer@gmail.com>
2015-04-09 07:59:25 +0300

    20150409 0758qtime added scoring functionality, newgame button, and centerline

--- Pong1/ViewController.m
+++ Pong1/ViewController.m
@@ -13,6 +13,7 @@
 
 @interface ViewController () <PaddleDelegate>
 @property (nonatomic, strong) UIDynamicAnimator *animator;
+@property (nonatomic, assign, readwrite) id<UIDynamicAnimatorDelegate> dynamicAnimatorDelegate;
 @property (nonatomic, strong) Ball *ball;
 @property (nonatomic, strong) Paddle *paddle1;
 @property (nonatomic, strong) Paddle *paddle2;
@@ -23,12 +24,22 @@
 @property CGFloat paddle2Y;
 @property CGFloat paddleDensity;
 
-
 @property CGPoint ballCenterPast;
 @property CGPoint ballCenter;
 @property NSMutableArray *ballCenters;
 @property int counter;
+
 @property CGPoint aiPaddleCenter;
+
+@property UILabel *player1ScoreLabel;
+@property UILabel *aiPlayerScoreLabel;
+@property CGFloat player1Score;
+@property CGFloat aiPlayerScore;
+
+@property (nonatomic, strong) UIButton *resetGameButton;
+
+@property NSTimer *ballTimer;
+@property NSTimer *scoreAndNewGameTimer;
 
 
 @property (nonatomic,strong) UICollisionBehavior *collider;
@@ -37,14 +48,23 @@
 
 
 
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    [super viewDidLoad];
+    [self allViewDidLoadExceptSuper];
+}
+
+-(void) allViewDidLoadExceptSuper {
+    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];\
+    self.animator.delegate = self.dynamicAnimatorDelegate;
     
     [self ballGenerator];
+    [self scoreLabelGenerator];
     
     UIBezierPath *centerLine = [UIBezierPath bezierPath];
     [[UIColor blackColor] setStroke];
@@ -98,24 +118,41 @@
     
     //mover for paddle2
     //methodology is: log ball position .1 seconds ago and move to match. Will tweak arbitrary .1 when testing game.
-//    [Ball addObserver:self forKeyPath:@"ballCenterPast" options:0 context:nil];
+    //    [Ball addObserver:self forKeyPath:@"ballCenterPast" options:0 context:nil];
     
     
     //pusher for starting ball
     self.pusher = [[UIPushBehavior alloc] initWithItems:@[self.ball] mode:UIPushBehaviorModeInstantaneous];
     self.pusher.pushDirection = CGVectorMake(0.5, 1.0);
     self.pusher.active = YES;
+    [self.animator addBehavior:self.pusher];
     
     //push is instantaneous, will only happen once
     
     
-    [self.animator addBehavior:self.pusher];
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-//    
-    NSTimer *ballTimer = [NSTimer scheduledTimerWithTimeInterval:.1 target:self selector:@selector(reportBallPosition) userInfo:nil repeats:YES];
     
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    //
+    _ballTimer = [NSTimer scheduledTimerWithTimeInterval:.2 target:self selector:@selector(reportBallPosition) userInfo:nil repeats:YES];
+    
+    _scoreAndNewGameTimer = [NSTimer scheduledTimerWithTimeInterval:.01 target:self selector:@selector(updateScoreAndNewGame) userInfo:nil repeats:YES];
+    
+    _resetGameButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [_resetGameButton addTarget:self action:@selector(resetGameButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    [_resetGameButton setTitle:@"Again?" forState:UIControlStateNormal];
+    _resetGameButton.frame = CGRectMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2, 100, 100);
 }
+
+#pragma mark - Ball Methods
+
+- (void) ballGenerator {
+    self.ball = [[Ball alloc] initWithFrame:CGRectMake(100,200,40,40)];
+    self.ball.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.ball];
+}
+
+#pragma mark - Paddle Methods incl AI Paddle w/ReportBallPosition
 
 - (void) paddle:(Paddle *)paddle didTryToPanWithOffset:(CGPoint)offset {
     CGPoint startingPoint = paddle.frame.origin;
@@ -132,28 +169,15 @@
     [self.animator updateItemUsingCurrentState:self.paddle1];
 }
 
-//- (void) paddle:(Paddle *)paddle aiControlledPaddleMove:(CGPoint)offset {
-//    CGPoint startingPoint = paddle.frame.origin;
-//    CGPoint new
-//}
-
-- (void) ballGenerator {
-    self.ball = [[Ball alloc] initWithFrame:CGRectMake(100,200,40,40)];
-    self.ball.backgroundColor = [UIColor redColor];
-    [self.view addSubview:self.ball];
-}
-
 -(void) reportBallPosition {
     self.ballCenter = self.ball.center;
     self.ballCenters = [[NSMutableArray alloc] init];
-//    if (self.ballCenters.count < 10) {
-        [self.ballCenters addObject:[NSValue valueWithCGPoint:self.ball.center]];
-//    } else {
-        NSValue *val = [self.ballCenters objectAtIndex:0];
-        self.ballCenterPast = [val CGPointValue];
-//        [self.ballCenters removeObject:0];
-    
-    CGPoint startingPoint = _paddle2.frame.origin;
+    //    if (self.ballCenters.count < 10) {
+    [self.ballCenters addObject:[NSValue valueWithCGPoint:self.ball.center]];
+    //    } else {
+    NSValue *val = [self.ballCenters objectAtIndex:0];
+    self.ballCenterPast = [val CGPointValue];
+    //        [self.ballCenters removeObject:0];
     
     CGFloat ballCenterX = self.ballCenterPast.x;
     
@@ -162,18 +186,83 @@
     if(CGRectContainsRect(self.view.bounds, potentialNewPaddleFrame)) {
         _paddle2.frame = potentialNewPaddleFrame;
     }
-//    self.paddle2.Center = paddle2.frame.origin;
+    //    self.paddle2.Center = paddle2.frame.origin;
     
     [self.animator updateItemUsingCurrentState:self.paddle2];
     
-//        NSLog(@"Current Ball Center: %@", [self.ballCenters objectAtIndex:9]);
-        NSLog(@"Ball Center Sent to AI Paddle: %@", [self.ballCenters objectAtIndex:0]);
-//    }
+    //        NSLog(@"Current Ball Center: %@", [self.ballCenters objectAtIndex:9]);
+    NSLog(@"Ball Center Sent to AI Paddle: %@", [self.ballCenters objectAtIndex:0]);
+    //    }
     
     NSLog(@"Ball Counter: %lu", (unsigned long)self.ballCenters.count);
 }
 
-#pragma mark - Key/Value Observing
+#pragma mark - Scoring and Pausing and NewGame functions
+
+- (void) updateScoreAndNewGame{
+    CGFloat p1PaddleY = CGRectGetMaxY(self.paddle1.frame);
+    CGFloat aiPaddleY = CGRectGetMaxY(self.paddle2.frame);
+    CGFloat ballCenterY = CGRectGetMaxY(self.ball.frame);
+    if(ballCenterY != 0) {
+        if (ballCenterY > p1PaddleY - 1) {
+            _aiPlayerScore++;
+            [self.animator removeAllBehaviors];
+            [self.aiPlayerScoreLabel removeFromSuperview];
+            [self scoreLabelGenerator];
+            [self.view addSubview:_resetGameButton];
+            [_scoreAndNewGameTimer invalidate];
+        } else if (ballCenterY < aiPaddleY + 1) {
+            _player1Score++;
+            [self.animator removeAllBehaviors];
+            [self.player1ScoreLabel removeFromSuperview];
+            [self scoreLabelGenerator];
+            [self.view addSubview:_resetGameButton];
+            [_scoreAndNewGameTimer invalidate];
+        }
+    }
+    //    _player1Score = _player1Score;
+    //    _aiPlayerScore = _aiPlayerScore;
+    
+    [self.animator updateItemUsingCurrentState:self.ball];
+    
+    NSLog(@"Player1 Score: %f, AI Score: %f", _player1Score, _aiPlayerScore);
+    
+}
+
+
+- (void) scoreLabelGenerator {
+    
+    UIView *centerLine = [[UIView alloc] initWithFrame:CGRectMake(0, self.view.bounds.size.height / 2, self.view.bounds.size.width, 1)];
+    centerLine.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:centerLine];
+    [self.view sendSubviewToBack:centerLine];
+    
+    CGFloat centerHeight = self.view.bounds.size.height / 2;
+    
+    _player1ScoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, centerHeight + 10, 100, 100)];
+    _player1ScoreLabel.text = [[NSString alloc]initWithFormat:@"P1:%.f",_player1Score];
+    
+    _aiPlayerScoreLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, centerHeight - 100, 100, 100)];
+    _aiPlayerScoreLabel.text = [[NSString alloc]initWithFormat:@"AI:%.f",_aiPlayerScore];
+    
+    
+    [self.view addSubview:_player1ScoreLabel];
+    [self.view addSubview:_aiPlayerScoreLabel];
+    
+    
+}
+
+-(void) resetGameButtonPressed{
+    NSArray *viewsToRemove = [self.view subviews];
+    for (UIView *v in viewsToRemove){
+        [v removeFromSuperview];
+    }
+    [self allViewDidLoadExceptSuper];
+}
+
+
+
+#pragma mark - Collision Behaviors
 
 - (void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p{
     
